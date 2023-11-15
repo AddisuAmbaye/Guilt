8d73f1a2592671b834f568196c629d29be16cbcd
addis <addeson3@gmail.com>
2023-11-15 14:55:18 +0300

    more crud

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { createProductCtrl,getProductsCtrl } from "../controllers/productsCtrl.js";
+import { 
+    createProductCtrl,
+    getProductsCtrl,
+    getProductCtrl,updateProductCtrl,
+    deleteProductCtrl
+ } from "../controllers/productsCtrl.js";
 import isLoggedIn from "../middlewares/isLoggedIn.js";
 const productsRouter = express.Router();
 
 productsRouter.post('/', isLoggedIn, createProductCtrl);
 productsRouter.get("/", getProductsCtrl);
-
+productsRouter.get("/:id", getProductCtrl);
+productsRouter.put("/:id", isLoggedIn, isAdmin, updateProductCtrl);
+productsRouter.delete("/:id/delete", isLoggedIn, isAdmin, deleteProductCtrl);
 
 export default productsRouter;
  
